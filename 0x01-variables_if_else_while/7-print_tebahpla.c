200b3f1dbd981e60fc49e01494acacce3cdf0280
omotomiwa26 <omotomiwa26@gmail.com>
2023-02-19 05:50:34 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,9 +8,12 @@
 
 int main(void)
 {
-int num;
-for (num = 0; num <= 9; num++)
-putchar(num + '0');
-putchar('\n');
+char alphabet = 'z';
+while (alphabet <= 'a')
+{
+putchar(alphabet);
+alphabet++;
 }
+putchar('\n');
 return (0);
+}
