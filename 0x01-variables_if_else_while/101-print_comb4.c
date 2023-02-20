8fb45b7ea316f12eaa4f78ea70dd9508c254d2dd
omotomiwa26 <omotomiwa26@gmail.com>
2023-02-19 16:51:04 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
  /**
- * main - program to solve combination of two digits
- *
- * Description: combination of two digits
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-int a, b;
+int a = 0;
+int b = 0;
+int c = 0;
 
+for (c = 0; c <= 9; c++)
+{
 for (b = 0; b <= 9; b++)
 {
 for (a = 0; a <= 9; a++)
 {
-if (!((a == b) || (b > a)))
+if (!((a == b) || (b == c) || (b > a) || (c > b)))
 {
+putchar(c + '0');
 putchar(b + '0');
 putchar(a + '0');
-if (!(a == 9 && b == 8))
+if (!(a == 9 && c == 7 && b == 8))
 {
 putchar(',');
 putchar(' ');
@@ -28,7 +31,7 @@ putchar(' ');
 }
 }
 }
-putchar('\n');
-
+}
+putchar ('\n');
 return (0);
 }
