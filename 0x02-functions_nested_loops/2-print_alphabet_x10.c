508c39710dc7f0b8e51d074972defee658e74f49
omotomiwa26 <omotomiwa26@gmail.com>
2023-02-21 20:19:25 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*print_alphabet_x10 - Entry point
+*
+* Return: void
+*/
+
+void print_alphabet_x10 main(void)
+{
+int i = 1;
+char alphabet;
+while (i < = 10)
+{
+alphabet = 'a';
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+_putchar('\n');
+i++;
+}
+}
