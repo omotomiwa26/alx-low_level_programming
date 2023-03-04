380c66b0f06dfaff4e9123efeafca8e72eb89548
omotomiwa26 <omotomiwa26@gmail.com>
2023-03-03 16:04:24 -0800

    function that changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,9 +7,9 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+char *string_toupper(char *z);
+char *cap_string(char *z);
+char *leet(char *z);
 
 
 #endif
