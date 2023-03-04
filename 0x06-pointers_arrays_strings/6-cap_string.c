e22c2b61cc15747a1a652b596eaee8f577d662ba
omotomiwa26 <omotomiwa26@gmail.com>
2023-03-04 06:11:32 -0800

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,26 +9,31 @@
 
 char *cap_string(char *str)
 {
-if (str[0] >= 97 && str[0] <= 122)
-str[0] -= 32;
-int i;
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] == ',' ||
-str[i] == ';' ||
-str[i] == ' ' ||
-str[i] == '\n' ||
-str[i] == '\t' ||
-str[i] == '.' ||
-str[i] == '!' ||
-str[i] == '?' ||
-str[i] == '"' ||
-str[i] == '(' ||
-str[i] == ')' ||
-str[i] == '{' ||
-str[i] == '}')
-if (str[i + 1] >= 97 && str[i + 1] <= 122)
-str[i + 1] -= 32;
-}
-return (0);
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 97 && str[i] <= 122))
+			i++;
+
+		if (str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0)
+			str[i] -= 32;
+
+		i++;
+	}
+
+	return (str);
 }
