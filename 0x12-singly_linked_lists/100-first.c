05eb6f8b0b3d51957f78eaeb9f4c89527d65f8f7
omotomiwa26 <omotomiwa26@gmail.com>
2023-03-31 00:07:00 -0700

    function that prints You're beat! and yet, you must allow, bore my house upon my back before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - program prints a sentence before the main function is executed
+ *
+ */
+
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
