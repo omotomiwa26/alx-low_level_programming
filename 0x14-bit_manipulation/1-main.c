9c1eaf4f1d385e4a2295b165f09626e8e9272b1f
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-09 07:40:22 -0700

    function that prints the binary representation of a number.

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -5,17 +5,18 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    print_binary(0);
-    printf("\n");
-    print_binary(1);
-    printf("\n");
-    print_binary(98);
-    printf("\n");
-    print_binary(1024);
-    printf("\n");
-    print_binary((1 << 10) + 1);
-    printf("\n");
-    return (0);
+print_binary(0);
+printf("\n");
+print_binary(1);
+printf("\n");
+print_binary(98);
+printf("\n");
+print_binary(1024);
+printf("\n");
+print_binary((1 << 10) + 1);
+printf("\n");
+return (0);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
