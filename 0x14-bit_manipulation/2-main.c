b99f3a2df9ba1bd147fecaf7285b76093b3e48bd
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-09 08:06:37 -0700

    function that returns the value of a bit at a given index.

--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    int n;
+int n;
 
-    n = get_bit(1024, 10);
-    printf("%d\n", n);
-    n = get_bit(98, 1);
-    printf("%d\n", n);
-    n = get_bit(1024, 0);
-    printf("%d\n", n);
-    return (0);
+n = get_bit(1024, 10);
+printf("%d\n", n);
+n = get_bit(98, 1);
+printf("%d\n", n);
+n = get_bit(1024, 0);
+printf("%d\n", n);
+return (0);
 }
