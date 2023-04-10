b65aebd7bdce5b0ce4eab868b2e4b93b44b2709a
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-09 21:34:16 -0700

    function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,6 +18,9 @@ if (!b)
 return (0);
 for (z = 0; z < length; z++)
 {
+if (b[z] < '0' || b[z] > '1')
+return (0);
+{
 if (b[z] == '1')
 {
 int_result += 1 << (length - z - 1);
@@ -25,6 +28,7 @@ int_result += 1 << (length - z - 1);
 else if (b[z] != '0')
 {
 return (0);
+}
 }
 }
 return (int_result);
