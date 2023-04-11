e591bd6bac264aa1bdcdfab103ee4f091afefff7
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-10 18:39:16 -0700

    function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - Function creates a file
+ * @filename: pointer to the name of file to create
+ * @text_content: pointer to string to wtite to file
+ * Return: 1 - Success
+ * Else: -1 - Error
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+    FILE *fp;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+
+
+	fp = fopen(filename, "w");
+	{
+	if (fp == NULL)
+		return (-1);
+	}
+	fprintf(fp, "%s", text_content);
+	fclose(fp);
+        
+	return (1);
+}
