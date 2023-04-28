0bbaa9ce89c950603da9d0681169c3c2aa987bf8
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-28 03:48:31 -0700

    program that copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void file_check(int file_from, int file_to, char *argv[]);
 
 #endif
