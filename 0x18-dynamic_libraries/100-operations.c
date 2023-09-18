dd2b93db945c0ba88fd8d4b5210ce90333a684b0
omotomiwa26 <omotomiwa26@gmail.com>
2023-09-18 23:12:56 +0000

    100_operations_c_file

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
+/**
+ * add - Functions adds two numbers.
+ * @a: Num1.
+ * @b: Num2.
+ * Return: result.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Function substracts two numbers.
+ * @a: Num1.
+ * @b: Num2
+ * Return: result.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - Function multiplies two numbers.
+ * @a: Num1.
+ * @b: Num2.
+ * Return: result.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Function divides two numbers.
+ * @a: Num1.
+ * @b: Num2.
+ * Return: result.
+ */
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: divide by a number greater than zero");
+	}
+	else
+	{
+		return (a / b);
 
+	}
+
+}
+/**
+ * mod - Function returns the remainder of two numbers
+ * @a: Num1.
+ * @b: Num2.
+ * Return: result.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
