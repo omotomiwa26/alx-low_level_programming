7ca810cdb23168b831882d089234407789d47871
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-18 04:11:07 +0000

    revised header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_sub_array(int *array, size_t start, size_t end);
+int jump_search(int *array, size_t size, int value);
 
 
 
